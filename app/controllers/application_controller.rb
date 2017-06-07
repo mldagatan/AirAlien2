2081c23be2c2df9b801eb55b281c5e4efada4899
Marc Dagatan <marc@vangardkreativ.com>
2017-06-07 20:42:07 +0800

    not signed in redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_paramters, if: :devise_controller?
 
+  def user_logged_in
+		if !user_signed_in?
+			redirect_to new_user_session_path, notice: "You need to sign in first."
+		end
+	end
+
   protected
   	def configure_permitted_paramters
   		devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
   		devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description, :email, :password])
   	end
+
 end
